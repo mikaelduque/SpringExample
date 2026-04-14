5ace0c50f4d3257c01520ddcb35b531bbfefece7
mikaelduque- <mikael.duque@aluno.senai.br>
2026-04-14 14:49:24 -0300

    service pauta

---
+++ controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/Services/pautaServices.java
@@ -0,0 +1,53 @@
+package com.senai.backend.controle_frequencia.Services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senai.backend.controle_frequencia.models.Pauta;
+import com.senai.backend.controle_frequencia.repositeres.pautaRepositories;
+
+@Service
+public class pautaServices {
+
+    @Autowired
+    private pautaRepositories pautaRepository;
+
+    public Long contarPautas() {
+        return pautaRepository.count();
+    }
+
+    public Pauta buscarPauta(Integer id) {
+        return pautaRepository.findById(id).get();
+    }
+
+    public List<Pauta> listarPautas(){
+        return pautaRepository.findAll();
+    }
+
+    public Boolean deletarPauta(Integer id) {
+        if (pautaRepository.existsById(id)) {
+            pautaRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public Pauta cadastrPauta(Pauta pauta) {
+        return pautaRepository.save(pauta);
+    }
+
+    public Pauta atualizarPauta(Integer id, Pauta pauta) {
+        Pauta pautaRecuperada = buscarPauta(id);
+        if (pautaRecuperada != null) {
+            pautaRecuperada.setId(id);
+            if (pauta.getTurma() != null) {
+                pautaRecuperada.setTurma(pauta.getTurma());
+            }
+            return pautaRepository.save(pautaRecuperada);
+        }
+        return null;
+
+    }
+}
